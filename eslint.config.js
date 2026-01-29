70fdedc21e7482c639ebfe0e9b7ba73f7bfa4f61
Ken Dearman <ken@lebaron.org.uk>
2026-01-29 20:50:13 +0000

    chore: ESLint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        // Foundry VTT Globals
+        foundry: "readonly",
+        game: "readonly",
+        canvas: "readonly",
+        ui: "readonly",
+        Hooks: "readonly",
+        FilePicker: "readonly",
+        AudioHelper: "readonly",
+        VideoHelper: "readonly",
+        PIXI: "readonly",
+        Actor: "readonly",
+        Item: "readonly",
+        Scene: "readonly",
+        Token: "readonly",
+        User: "readonly",
+        ChatMessage: "readonly",
+        Macro: "readonly",
+        Roll: "readonly",
+        Config: "readonly",
+        // Hosting & Module Globals
+        ForgeVTT: "readonly",
+        Sequencer: "readonly",
+        Trowel: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-debugger": "warn",
+      "no-console": "off" // Set to "warn" if you want to find left-over logs
+    }
+  }
+];
